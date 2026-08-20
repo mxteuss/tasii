bca18288001c30a15cd3f76486a2f3d96ab733d2
MATEUS DE SANTANA SANTOS <mateussantos007@cs.cruzeirodosul.edu.br>
2026-08-20 10:30:52 -0300

    Tests de tamanho título

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -20,4 +20,16 @@ describe('Suíte de Testes - Validador de Chamados (Service Desk)', () => {
             validarChamado("   ");
         }).toThrow("O título do chamado não pode ser vazio.");
     });
+
+    test('Deve lançar erro se o título tiver menos que 5 caracteres', () => {
+        expect(() => {
+            validarChamado("oioi");
+        }).toThrow("O título do chamado não pode ter menos que 5 caracteres.")
+    })
+
+       test('Deve lançar erro se o título tiver mais que 100 caracteres', () => {
+        expect(() => {
+            validarChamado("oioi");
+        }).toThrow("O título do chamado não pode ter mais que 100 caracteres.")
+    })
 });
