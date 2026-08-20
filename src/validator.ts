efb55d57b3ecbf7d9424cf2aa255ccaba86dd13a
MATEUS DE SANTANA SANTOS <mateussantos007@cs.cruzeirodosul.edu.br>
2026-08-20 10:27:22 -0300

    Validações de titulo

--- src/validator.ts
+++ src/validator.ts
@@ -1,7 +1,16 @@
 function validarChamado(titulo: string): boolean {
-    if (!titulo) {
+    if (!titulo || titulo.trim() == "") {
         throw new Error("O título do chamado não pode ser vazio.");
     }
+
+    else if(titulo.length < 5){
+        throw new Error("O título deve ter no mínimo 5 caracteres");
+    }
+
+    else if (titulo.length > 100){
+        throw new Error("O titulo deve posssuir no máximo 100 caracteres");
+        }
+
 
     return true;
 }
