e5d5c2431c1128fb86770a33e107754773a04616
MATEUS DE SANTANA SANTOS <mateussantos007@cs.cruzeirodosul.edu.br>
2026-08-20 09:40:55 -0300

    first commit

---
+++ src/validator.ts
@@ -0,0 +1,9 @@
+function validarChamado(titulo: string): boolean {
+    if (!titulo || titulo.trim() === "") {
+        throw new Error("O título do chamado não pode ser vazio.");
+    }
+
+    return true;
+}
+
+export default validarChamado;
